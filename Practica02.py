d237fc89eca60f0db21e424edb69a8d6193af7a1
Ricardo Marquez <lasuerteloestodo@gmail.com>
2019-06-25 08:31:26 -0500

    pregunta02 finalizada

--- Practica02.py
+++ Practica02.py
@@ -12,3 +12,23 @@
 # promedio al finalizar el curso para poder garantizar que me 
 # podré matricular en un curso lo cual este es su pre-requisito
 
+pc1=int(input("ingrese la nota de la pc1"))
+pc2=int(input("ingrese la nota de la pc2"))
+trabajo=int(input("ingrese la nota del trabajo"))
+expar=int(input("ingrese la nota del parcial"))
+exfi=int(input("ingrese la nota del final"))
+
+promprac=(((pc1+pc2)*0.5))*0.6
+trab=trabajo*0.4
+prom=(promprac+trab)*0.5
+par=expar*0.25
+fin=exfi*0.25
+
+notafinal=prom+par+fin
+
+print(notafinal)
+
+if notafinal>10:
+    print("Aprobó el curso")
+else:
+    print("Desaprobó el curso")
